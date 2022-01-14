5f9ad6509bb739ffc8070f6a692aa80c5f3e5ca9
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-01-14 23:12:22 +0000

    remove unnecessary scripts and files for project

--- reservations/forms.py
+++ reservations/forms.py
@@ -8,15 +8,3 @@ class ReservationForm(ModelForm):
         model = Reservation
         fields = ['first_name', 'last_name', 'user_email', 'user_phone',
                   'date', 'time', 'for_how_many', 'mixologist']
-
-        # widgets = {
-        #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'user_email': forms.EmailInput(attrs={'class': 'form-control'}),
-        #     'user_phone': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'date': forms.DateInput(attrs={
-        #         'class': 'form-control', 'placeholder': 'YYYY/MM/DD'}),
-        #     'time': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'for_how_many': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'mixologists': forms.Select(attrs={'class': 'form-control'}),
-        # }

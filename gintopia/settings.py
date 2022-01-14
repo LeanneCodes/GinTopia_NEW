542430d76d24e6462bb6bb22328eb6d8ae029d55
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-01-14 20:52:15 +0000

    adjust styling to form page

--- gintopia/settings.py
+++ gintopia/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'reservations',
+    'widget_tweaks',
 ]
 
 SITE_ID = 1
--- reservations/forms.py
+++ reservations/forms.py
@@ -9,14 +9,14 @@ class ReservationForm(ModelForm):
         fields = ['first_name', 'last_name', 'user_email', 'user_phone',
                   'date', 'time', 'for_how_many', 'mixologist']
 
-        widgets = {
-            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'user_email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'user_phone': forms.TextInput(attrs={'class': 'form-control'}),
-            'date': forms.DateInput(attrs={
-                'class': 'form-control', 'placeholder': 'YYYY/MM/DD'}),
-            'time': forms.TextInput(attrs={'class': 'form-control'}),
-            'for_how_many': forms.TextInput(attrs={'class': 'form-control'}),
-            'mixologists': forms.Select(attrs={'class': 'form-control'}),
-        }
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'user_email': forms.EmailInput(attrs={'class': 'form-control'}),
+        #     'user_phone': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'date': forms.DateInput(attrs={
+        #         'class': 'form-control', 'placeholder': 'YYYY/MM/DD'}),
+        #     'time': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'for_how_many': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'mixologists': forms.Select(attrs={'class': 'form-control'}),
+        # }
